b561d6d83374ccdfa5926729eadcf32c2d5134cf
Rui Andrada <shingonoide@gmail.com>
2011-03-24 22:10:00 -0300

    Updating testing.rb simple test script

--- testing.rb
+++ testing.rb
@@ -5,47 +5,17 @@ require 'dm-dh_api-adapter'
 
 DataMapper.setup(:default, :adapter => :dh_api, :api_key => '6SHU5P2HLDAYECUM')
 
-class Domain
-  include DataMapper::Resource
-  property :fastcgi, Boolean
-  property :hosting_type, String
-  property :xcache, Boolean
-  property :php_fcgid, Boolean
-  property :security, Boolean
-  property :unique_ip, String
-  property :domain, String
-  property :account, String
-  property :type, String
-  property :passanger, Boolean
-  property :path, String
-  property :home, String
-  property :user, String
-  property :www_or_not, String
-end
-
-class DNS
-  include DataMapper::Resource
-
-  property :comment, String
-  property :zone, String
-  property :type, String
-  property :editable, Boolean
-  property :value, String
-  property :record, String
-
-end
-
-class DhUser
-  include DataMapper::Resource
-
-  # storage_names[:default] = 'users'
-end
+require 'dm-dh_api-adapter/models'
+require 'pp'
+include DataMapper::DhApi::Models
 
 domains = Domain.all
-dnses_editables =  DNS.all(:editable => true)
+dnses_editables = DNS.all(:editable => true)
 dnses_not_editables = DNS.all(:editable => false)
-users = DhUser.all
+users = User.all
+pp Domain.all
 
+puts "#{Domain.last.domain}"
 puts "There's #{domains.size} domains records"
 
 puts "There's #{dnses_editables.size} editables dnses records"
