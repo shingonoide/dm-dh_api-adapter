4ffac0d6ea7628a4bbff0d585cebb7a47e243a97
Rui Andrada <shingonoide@gmail.com>
2011-03-24 22:08:32 -0300

    Added basic models

---
+++ lib/dm-dh_api-adapter/models.rb
@@ -0,0 +1,39 @@
+module DataMapper
+  module DhApi
+    module Models
+      class DNS
+        include DataMapper::Resource
+
+        property :comment, String
+        property :zone, String
+        property :type, String
+        property :editable, Boolean
+        property :value, String
+        property :record, String
+
+      end
+
+      class Domain
+        include DataMapper::Resource
+        property :fastcgi, Boolean
+        property :hosting_type, String
+        property :xcache, Boolean
+        property :php_fcgid, Boolean
+        property :security, Boolean
+        property :unique_ip, String
+        property :domain, String
+        property :account, String
+        property :type, String
+        property :passanger, Boolean
+        property :path, String
+        property :home, String
+        property :user, String
+        property :www_or_not, String
+      end
+
+      class User
+        include DataMapper::Resource
+      end
+    end
+  end
+end
